95f38d0c6bae036b18d9f358d8ce958d735a58d8
Jorge Torres <jorgeatm338@gmail.com>
2026-09-04 01:56:27 -0400

    Se creo el html de pedidos, views de pedidos con datos de ejemplo

--- pedidos/views.py
+++ pedidos/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+def pedidos(request):
+    pedido_ejemplo = {
+        'id_despacho': 'DSP001',
+        'cliente': 'Juan Perez',
+        'fecha_compra': '10-09-2026',
+        'estado_despacho': 'En tránsito',
+        'productos': [
+            {'nombre': 'Procesador Ryzen 5 5600X', 'cantidad': 1 },
+            {'nombre': 'Tarjeta de video NVIDIA GeForce RTX 3060', 'cantidad': 1 },
+            {'nombre': 'Memoria RAM Corsair Vengeance 16GB', 'cantidad': 2 },
+        ],
+        'fecha_entrega': '20-09-2026',
+        
+    }
+    contexto = {
+        'titulo': 'Seguimiento de pedidos',
+        'pedido': pedido_ejemplo,
+    }
+    return render(request, 'pedidos/pedidos.html', contexto)
